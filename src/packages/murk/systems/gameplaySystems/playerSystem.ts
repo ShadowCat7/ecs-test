af6b40f62aa45b3f5ea3ff4540018cf517d749bc
ShadowCat7 <lukasattridge@gmail.com>
2026-04-13 19:57:04 -0700

    move player around not on a grid

--- src/packages/murk/systems/gameplaySystems/playerSystem.ts
+++ src/packages/murk/systems/gameplaySystems/playerSystem.ts
@@ -1,69 +1,59 @@
-import { addEntity } from "../../../sanguine/entities/entities.js";
+import { addEntity, getComponents, getEntity, removeEntity } from "../../../sanguine/entities/entities.js";
 import { Component, Message, System } from "../../../sanguine/types.js";
 import { createEntity } from "../../../sanguine/entities/entity.js";
 import { getPrefab } from "../../../sanguine/prefabs/prefabs.js";
 import { PhysicsComponent } from "../../components/physicsComponent.js";
 import { getControl } from "../../controls.js";
+import { projectVector, setMagnitude, setMagnitudeVector, subtractVector } from "../../../sanguine/util/vector.js";
+import { LevelDownMessage, LevelUpMessage, PlayerBouncedMessage, PlayerEatenMessage, PlayerEatMessage } from "./types.js";
 import { createTimer } from "../../../sanguine/util/timer.js";
-
-const MOVEMENT_DURATION = 0.2;
 
 export const playerSystem = (
     messager: (message: Message) => void,
 ): System => {
-    let moving = false;
-    let movementTimer = createTimer(MOVEMENT_DURATION, () => {
-        moving = false;
-    });
-
     // create player
-    const player = createEntity(200, 200, getPrefab('head'));
+    const player = createEntity(300, 300, getPrefab('head'));
     const physics = player.getComponent<PhysicsComponent>('physics')!;
     physics.velocityX = 0;
-    physics.velocityY = 0;
+    physics.velocityY = physics.topSpeed;
     addEntity(player);
 
     return {
-        triggers: [],
+        triggers: [
+        ],
         componentType: 'player',
         process: (components: Component[], elapsedTime: number) => {
-            if (player) {
-                movementTimer.update(elapsedTime);
+            if (!player) return;
+            const physics = player.getComponent<PhysicsComponent>('physics')!;
 
-                const physics = player.getComponent<PhysicsComponent>('physics')!;
+            let [velocityX, velocityY] = setMagnitude(physics.velocityX, physics.velocityY, 1);
 
-                // check held down vs pressing repeatedly
-                if (moving) {
-                    return;
-                }
+            let newX = 0;
+            let newY = 0;
 
-                const { x, y } = player;
-                let directionX = 0;
-                let directionY = 0;
+            if (getControl('left').current) {
+                newX -= elapsedTime * 10;
+            }
+            if (getControl('right').current) {
+                newX += elapsedTime * 10;
+            }
+            if (getControl('up').current) {
+                newY -= elapsedTime * 10;
+            }
+            if (getControl('down').current) {
+                newY += elapsedTime * 10;
+            }
 
-                if (getControl('left').current) {
-                    directionX--;
-                } else if (getControl('right').current) {
-                    directionX++;
-                } else if (getControl('up').current) {
-                    directionY--;
-                } else if (getControl('down').current) {
-                    directionY++;
-                }
-
-                if (directionX !== 0) {
-                    physics.moveToX = x + directionX * 30;
-                    physics.moveToY = y;
-                } else if (directionY !== 0) {
-                    physics.moveToX = x;
-                    physics.moveToY = y + directionY * 30;
-                }
-
-                if (directionX || directionY) {
-                    moving = true;
-                    movementTimer.reset();
+            if (!newX && !newY) {
+                physics.velocityX = 0;
+                physics.velocityY = 0;
+            } else {
+                const [newVelocityX, newVelocityY] = setMagnitude(velocityX + newX, velocityY + newY, physics.topSpeed * player.scale);
+                if (newVelocityX !== 0 || newVelocityY !== 0) {
+                    physics.velocityX = newVelocityX;
+                    physics.velocityY = newVelocityY;
                 }
             }
-        }
+        };
     };
-}
+};
